5eba68c3555ae5f5191caa9676e3cd823f26922f
wengwi <wilson.weng@cargosmart.com>
2021-02-02 16:22:37 +0800

    4th

--- src/main/java/com/example/wilson/controller/HelloWorldController.java
+++ src/main/java/com/example/wilson/controller/HelloWorldController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @RequestMapping("/say")
     public String say(){
-        return " commit";
+        return " nihao  commit";
     }
 }
